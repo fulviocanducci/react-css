de6c47b1ef41ee4c6e61b09a090600bc90a7c437
Fúlvio <fulviocanducci@hotmail.com>
2020-08-04 14:29:35 -0300

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+function App() {
+  return (
+    <div className="container">
+      <header></header>
+      <main></main>
+      <aside></aside>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
